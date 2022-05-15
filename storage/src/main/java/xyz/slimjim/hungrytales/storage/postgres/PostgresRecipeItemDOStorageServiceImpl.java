77caccb6c23da8d777af87e70b41279229d8fb21
James Dyke <james.dyke91@gmail.com>
2022-05-15 14:56:12 +0200

    Naive approach to 2NF query

--- storage/src/main/java/xyz/slimjim/hungrytales/storage/postgres/PostgresRecipeItemDOStorageServiceImpl.java
+++ storage/src/main/java/xyz/slimjim/hungrytales/storage/postgres/PostgresRecipeItemDOStorageServiceImpl.java
@@ -5,8 +5,12 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import xyz.slimjim.hungrytales.common.cooking.CookingStep;
+import xyz.slimjim.hungrytales.common.item.IngredientItem;
 import xyz.slimjim.hungrytales.common.item.RecipeItem;
 import xyz.slimjim.hungrytales.storage.service.ItemStorageService;
+
+import java.util.List;
 
 @Repository
 public class PostgresRecipeItemDOStorageServiceImpl implements ItemStorageService<RecipeItem> {
@@ -23,6 +27,10 @@ public class PostgresRecipeItemDOStorageServiceImpl implements ItemStorageServic
     @Override
     public RecipeItem get(int id) {
         RecipeItem item = jdbcTemplate.queryForObject("select * from recipe where id = ?", BeanPropertyRowMapper.newInstance(RecipeItem.class), id);
+        List<IngredientItem> recipeIngredients = jdbcTemplate.query("select name, unit, amount from recipe_ingredients where recipe_id = ?", new BeanPropertyRowMapper<>(IngredientItem.class), id);
+        List<CookingStep> cookingSteps = jdbcTemplate.query("select step_number, instruction from recipe_instructions where recipe_id = ?", new BeanPropertyRowMapper<>(CookingStep.class), id);
+        item.setInstructions(cookingSteps);
+        item.setIngredients(recipeIngredients);
         return item;
     }
 
