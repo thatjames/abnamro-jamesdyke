82a38f3387f542a949133c20c0625573dca09af2
James Dyke <james.dyke91@gmail.com>
2022-05-17 15:59:03 +0200

    CORS components

--- src/main/java/xyz/slimjim/hungrytales/HungrytalesApplication.java
+++ src/main/java/xyz/slimjim/hungrytales/HungrytalesApplication.java
@@ -1,7 +1,15 @@
 package xyz.slimjim.hungrytales;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HungrytalesApplication {
@@ -10,4 +18,17 @@ public class HungrytalesApplication {
         SpringApplication.run(HungrytalesApplication.class, args);
     }
 
+    private static final Logger log = LoggerFactory.getLogger(HungrytalesApplication.class);
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String corsHostsConfig = System.getProperty("cors.hosts") == null ? "*" : System.getProperty("cors.hosts");
+                log.debug(String.format("set cors.hosts %s", corsHostsConfig));
+                registry.addMapping("/**").allowedOrigins(corsHostsConfig.split(","));
+            }
+        };
+    }
 }
